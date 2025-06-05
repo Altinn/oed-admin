05dfd1ca09ecb4d3317b8e58222c2fc1d22e7db9
Kurt Monge <kurt.monge@knowit.com>
2025-06-05 10:28:28 +0200

    1239: Første sjekkin

---
+++ src/oed-admin/Extensions/StringEx.cs
@@ -0,0 +1,10 @@
+﻿namespace oed_admin.Extensions
+{
+    public static class StringEx
+    {
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+    }
+}
