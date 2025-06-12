7b53ac931c450c8e3f9fb147e070ed1c35cf7e2b
Lars Olav Kjølstad <lars.olav.kjolstad@digdir.no>
2025-06-12 09:32:52 +0200

    small fix

--- src/oed-admin/Features/Endpoints.cs
+++ src/oed-admin/Features/Endpoints.cs
@@ -19,12 +19,14 @@ public static class Endpoints
         return group;
     }
 
-    public static void MapInstanceEndpoints(this WebApplication app)
+    public static RouteGroupBuilder MapInstanceEndpoints(this WebApplication app)
     {
         var group = app.MapGroup("/api/instance");
         //.RequireAuthorization();
 
         group.MapGet("/{instanceOwnerPartyId:int}/{instanceGuid:guid}", Instance.GetInstance.Endpoint.Get);
         group.MapGet("/{instanceOwnerPartyId:int}/{instanceGuid:guid}/data/{dataGuid:guid}", Instance.GetInstanceData.Endpoint.Get);
+
+        return group;
     }
 }
