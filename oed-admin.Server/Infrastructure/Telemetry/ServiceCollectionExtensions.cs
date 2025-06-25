dcff6bf6f2d11e55adbd901ea67f49d2ed42d9d9
DanRJ <daniel.rustadj@gmail.com>
2025-06-25 16:12:03 +0200

    Change OpenTelemtry logging

--- oed-admin.Server/Infrastructure/Telemetry/ServiceCollectionExtensions.cs
+++ oed-admin.Server/Infrastructure/Telemetry/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Azure.Monitor.OpenTelemetry.Exporter;
+﻿using Azure.Monitor.OpenTelemetry.AspNetCore;
+using Azure.Monitor.OpenTelemetry.Exporter;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
@@ -9,31 +10,38 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddTelemetry(this IServiceCollection services, IConfiguration config)
     {
-        var ai_connstr = config.GetValue<string>("APPLICATIONINSIGHTS_CONNECTION_STRING", string.Empty);
-        if (!string.IsNullOrEmpty(ai_connstr))
-        {
-            services.AddOpenTelemetry()
-                .ConfigureResource(resource => resource
-                    .AddService("oed-admin-api")
-                )
-                .WithTracing(tracing => tracing
-                    .AddAspNetCoreInstrumentation()
-                    .AddHttpClientInstrumentation()
-                    .AddEntityFrameworkCoreInstrumentation()
-                    .AddAzureMonitorTraceExporter(o =>
-                    {
-                        o.ConnectionString = ai_connstr;
-                    })
-                )
-                .WithMetrics(metrics => metrics
-                    .AddAspNetCoreInstrumentation()
-                    .AddRuntimeInstrumentation()
-                    .AddAzureMonitorMetricExporter(o =>
-                    {
-                        o.ConnectionString = ai_connstr;
-                    })
-                );
-        }
+        //var ai_connstr = config.GetValue<string>("APPLICATIONINSIGHTS_CONNECTION_STRING", string.Empty);
+        //if (!string.IsNullOrEmpty(ai_connstr))
+        //{
+        //    services.AddOpenTelemetry()
+        //        .ConfigureResource(resource => resource
+        //            .AddService("oed-admin-api")
+        //        )
+        //        .WithTracing(tracing => tracing
+        //            .AddAspNetCoreInstrumentation()
+        //            .AddHttpClientInstrumentation()
+        //            .AddEntityFrameworkCoreInstrumentation()
+        //            .AddAzureMonitorTraceExporter(o =>
+        //            {
+        //                o.ConnectionString = ai_connstr;
+        //            })
+        //        )
+        //        .WithMetrics(metrics => metrics
+        //            .AddAspNetCoreInstrumentation()
+        //            .AddRuntimeInstrumentation()
+        //            .AddAzureMonitorMetricExporter(o =>
+        //            {
+        //                o.ConnectionString = ai_connstr;
+        //            })
+        //        );
+        //}
+
+        services.AddOpenTelemetry()
+            .ConfigureResource(resource => resource
+                .AddService("oed-admin-api")
+            )
+            .WithTracing(tracing => tracing.AddEntityFrameworkCoreInstrumentation())
+            .UseAzureMonitor();
 
         return services;
     }
