80a8884741ff97e724908c41e4b0e4a9af193d1f
Kurt70 <kurt.monge@gmail.com>
2025-06-26 13:14:12 +0200

    Endret katalognavn, da den ble excludert

---
+++ oed-admin.Server/Features/Debuging/ExceptionRaiser/Endpoint.cs
@@ -0,0 +1,13 @@
+﻿using oed_admin.Server.Infrastructure.Altinn;
+using oed_admin.Server.Infrastructure.Database.Oed;
+
+namespace oed_admin.Server.Features.Debug.ExceptionRaiser
+{
+    public class Endpoint
+    {
+        public static async Task<IResult> Get()
+        {
+            throw new Exception("Test exception");
+        }
+    }
+}
