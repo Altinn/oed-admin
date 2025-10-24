b9d2752ce52e47824ca593050dd7f83a4252a173
DanRJ <daniel.rustadj@gmail.com>
2025-10-24 17:02:21 +0200

    Add backend for district courts

---
+++ oed-admin.Server/Features/DistrictCourts/GetDistrictCourts/Endpoint.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using oed_admin.Server.Infrastructure.Database.Oed;
+
+namespace oed_admin.Server.Features.DistrictCourts.GetDistrictCourts;
+
+public static class Endpoint
+{
+    public static async Task<IResult> Get(
+       [FromServices] OedDbContext dbContext)
+    {
+        var districtCourts = await dbContext
+            .Estate
+            .AsNoTracking()
+            .Select(e => e.DistrictCourtName)
+            .Where(dc => dc != null)
+            .Distinct()
+            .OrderBy(dc => dc)
+            .ToListAsync();
+
+        return TypedResults.Ok(new Response(districtCourts!));
+    }
+}
